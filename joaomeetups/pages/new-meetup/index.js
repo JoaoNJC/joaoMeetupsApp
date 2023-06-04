4e733b5db9524ee56d9bc19f9339470bfff37bc2
Joao Camba <joaocamba@tests-iMac.local>
2023-06-04 22:08:23 +0100

    Refactored index.js

--- joaomeetups/pages/new-meetup/index.js
+++ joaomeetups/pages/new-meetup/index.js
@@ -1,29 +1,27 @@
-import NewMeetupForm from '../../components/meetups/NewMeetupForm'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
-const NewMeetupPage = () => {
-  const router = useRouter()
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
-  async function addMeetupHandler(enteredData) {
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
     const response = await fetch('/api/new-meetup', {
       method: 'POST',
-      body: JSON.stringify(enteredData),
+      body: JSON.stringify(enteredMeetupData),
       headers: {
         'Content-Type': 'application/json'
       }
     });
 
     const data = await response.json();
+
     console.log(data);
 
     router.push('/');
-
-
   }
 
-  return (
-    <NewMeetupForm onAddMeetup={addMeetupHandler} />
-  )
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
 }
 
 export default NewMeetupPage;
